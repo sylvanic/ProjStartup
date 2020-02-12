2063969421d5dc7df9a7182882adc92797385ac1
Filippo Maria Leonardi <filippoml@yahoo.it>
2020-02-12 15:52:37 +0100

    Empty Project Upload

---
+++ FilippoWorld/Source/FilippoWorld/FilippoWorldGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FilippoWorldGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FILIPPOWORLD_API AFilippoWorldGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
